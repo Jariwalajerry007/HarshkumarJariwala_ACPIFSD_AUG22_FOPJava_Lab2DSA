2897137f7412141ac74b0709438759773feedaa7
Harshkumar Anilbhai Jariwala <109514142+Jariwalajerry007@users.noreply.github.com>
2022-10-10 22:08:41 +0530

    DRIVER READY

--- Week5FOPdsaLab2/srcDenomination/com/fopdsa/lab2Denomination/main/Main.java
+++ Week5FOPdsaLab2/srcDenomination/com/fopdsa/lab2Denomination/main/Main.java
@@ -1,10 +1,28 @@
 package com.fopdsa.lab2Denomination.main;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Denomination denomination = new Denomination();
+		try (Scanner sc = new Scanner(System.in)) {
+			int noofdenomie;
+			System.out.println("Enter the size of Currency Denominations");
+			noofdenomie = sc.nextInt();
+			System.out.println("Enter the Currency Denomination Value");
+			
+			//int valueOfdenomie;
+			int[] value = new int[noofdenomie];
+			for (int i = 0; i < noofdenomie; i++) {
+				value[i] = sc.nextInt();
 
+			}
+			System.out.println("Enter the amount you want to pay");
+			int amount = sc.nextInt();
+			denomination.bubbleSort(value);
+			denomination.noofNotes(value, amount);
+		}
 	}
 
 }
