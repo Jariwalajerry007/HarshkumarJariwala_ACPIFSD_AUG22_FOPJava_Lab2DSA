39968484f44bc7d9bd2d4bfcf4df1364b29556b9
Harshkumar Anilbhai Jariwala <109514142+Jariwalajerry007@users.noreply.github.com>
2022-10-10 22:06:03 +0530

    METHOD COMPLETED

--- Week5FOPdsaLab2/srcDenomination/com/fopdsa/lab2Denomination/methods/Denomination.java
+++ Week5FOPdsaLab2/srcDenomination/com/fopdsa/lab2Denomination/methods/Denomination.java
@@ -1,5 +1,36 @@
 package com.fopdsa.lab2Denomination.methods;
 
 public class Denomination{
+	public void noofNotes(int[] count, int amount) {
+		int[] result = new int[count.length];
+		for (int i = 0; i < count.length; i++) {
+			if (count[i] <= amount) {
+				result[i] = amount / count[i];
+				amount = amount % count[i];
+			}
+		}
+		if (amount > 0) {
+			System.out.println("Amount cannot be made with Denominaion");
+		} else {
+			System.out.println("Your payment approach in order to give minimum number of notes will be");
+			for (int i = 0; i < result.length; i++) {
+				if (result[i] != 0) {
+					System.out.println(count[i] + ":" + result[i]);
+				}
+			}
+		
+		}
+	}
 
+	public void bubbleSort(int[] Currency) {
+		for (int i = 0; i < Currency.length - 1; i++) {
+			for (int j = 0; j < Currency.length - i - 1; j++) {
+				if (Currency[j] < Currency[j + 1]) {
+					int temp = Currency[j];
+					Currency[j] = Currency[j + 1];
+					Currency[j + 1] = temp;
+				}
+			}
+		}
+	}
 }
